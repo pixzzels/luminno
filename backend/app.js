46f3fe82e07c29e5c314c7607bae55867fa7512f
Ellen Park <paarkellen@gmail.com>
2021-04-22 12:29:45 -0400

    created general error handling

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const { environment } = require('./config');
+const { ValidationError } = require('sequelize');
 const isProduction = environment === 'production';
 const app = express();
 const routes = require('./routes');
@@ -36,6 +37,38 @@ app.use(
     },
   })
 );
+
+// Catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, req, res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
 app.use(routes);
 
 module.exports = app;
