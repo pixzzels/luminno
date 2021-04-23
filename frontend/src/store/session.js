9d7d335ea90ceb0d1aebd1f5e795a3a84b7b98ae
Ellen Park <paarkellen@gmail.com>
2021-04-23 18:30:07 -0400

    working nav bar

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -53,6 +53,14 @@ export const signup = (user) => async (dispatch) => {
   return res;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
