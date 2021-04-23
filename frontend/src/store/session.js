d57fd7a048062dcc52806001a2078a1742c0dbaf
Ellen Park <paarkellen@gmail.com>
2021-04-23 17:49:36 -0400

    signup page completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -25,16 +25,32 @@ export const restoreUser = () => async (dispatch) => {
 
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
-  const response = await csrfFetch('/api/session', {
+  const res = await csrfFetch('/api/session', {
     method: 'POST',
     body: JSON.stringify({
       credential,
       password,
     }),
   });
-  const data = await response.json();
+
+  const data = await res.json();
   dispatch(setUser(data.user));
-  return response;
+  return res;
+};
+
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const res = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await res.json();
+  dispatch(setUser(data.user));
+  return res;
 };
 
 const initialState = { user: null };
