794bd4264d45396914b8fc998327e626481fd0ef
Ellen Park <paarkellen@gmail.com>
2021-04-27 16:19:58 -0400

    created styling for categories bar

--- frontend/src/components/Category/Category.js
+++ frontend/src/components/Category/Category.js
@@ -1,45 +1,13 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-import { getCategories } from '../../store/listings'
+import './Category.css'
 
-function Category() {
-  // const { id } = useParams();
-  const category = useSelector(state => {
-    // creates an array of the object values
-    const listings = Object.values(state.listings)
-    console.log("listings", listings)
-    return listings;
-    // return state.listings.list
-    // return state.category.list?.map(id => state.category[id]);
-  });
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getCategories())
-  }, [dispatch]);
-
-  if (!category) {
-    return null;
-  }
+function Category({ category }) {
 
   return (
-    <div>
-      {category.map((category) => {
-        return (
-          <NavLink key={category.name} to={`category/${category.id}`}>
-
-            <div>
-              <div className="primary-text">{category.name}</div>
-
-            </div>
-
-          </NavLink>
-        );
-      })}
-    </div>
+    <NavLink className="category-navLink" key={category.name} to={`category/${category.id}`}>
+      {category.name}
+    </NavLink>
   )
 }
 
